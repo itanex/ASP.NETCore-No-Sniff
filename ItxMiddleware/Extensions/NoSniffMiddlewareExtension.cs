b572682a49ef2001c7045454c0ef27215c4d6e7e
Bryan Wood <itanex@gmail.com>
2019-04-23 11:08:48 -0700

    Initial Library Project w/ No-Sniff Middleware

---
+++ ItxMiddleware/Extensions/NoSniffMiddlewareExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using ItxMiddleware.Middleware;
+
+namespace ItxMiddleware.Extensions
+{
+    public static class NoSniffMiddlewareExtension
+    {
+        public static IApplicationBuilder UseNoSniffMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<NoSniffMiddleware>();
+        }
+    }
+}
---
+++ ItxMiddleware/Middleware/NoSniffMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ItxMiddleware.Middleware
+{
+    public class NoSniffMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public NoSniffMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+
+            await next(context);
+        }
+    }
+}
